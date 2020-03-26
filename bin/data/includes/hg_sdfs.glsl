ced8efd46be97804eb60d65aaad80dfb27dea53c
connorbell <connor.n.bell@gmail.com>
2020-03-26 09:13:07 -0400

    Added shader includes

---
+++ bin/data/includes/hg_sdfs.glsl
@@ -0,0 +1,125 @@
+////////////////////////////////////////////////////////////////
+//
+//                           HG_SDF
+//
+//     GLSL LIBRARY FOR BUILDING SIGNED DISTANCE BOUNDS
+//
+//     version 2016-01-10
+//
+//     Check http://mercury.sexy/hg_sdf for updates
+//     and usage examples. Send feedback to spheretracing@mercury.sexy.
+//
+//     Brought to you by MERCURY http://mercury.sexy
+//
+//
+//
+// Released as Creative Commons Attribution-NonCommercial (CC BY-NC)
+//
+////////////////////////////////////////////////////////////////
+#define PHI (sqrt(5)*0.5 + 0.5)
+//
+// "Generalized Distance Functions" by Akleman and Chen.
+// see the Paper at https://www.viz.tamu.edu/faculty/ergun/research/implicitmodeling/papers/sm99.pdf
+//
+// This set of constants is used to construct a large variety of geometric primitives.
+// Indices are shifted by 1 compared to the paper because we start counting at Zero.
+// Some of those are slow whenever a driver decides to not unroll the loop,
+// which seems to happen for fIcosahedron und fTruncatedIcosahedron on nvidia 350.12 at least.
+// Specialized implementations can well be faster in all cases.
+//
+float fPlane(vec3 p, vec3 n, float distanceFromOrigin) {
+	return dot(p, n) + distanceFromOrigin;
+}
+
+float vmax(vec3 v) {
+	return max(max(v.x, v.y), v.z);
+}
+
+float fBox(vec3 p, vec3 b) {
+	vec3 d = abs(p) - b;
+	return length(max(d, vec3(0))) + vmax(min(d, vec3(0)));
+}
+
+const vec3 GDFVectors[19] = vec3[](
+	normalize(vec3(1, 0, 0)),
+	normalize(vec3(0, 1, 0)),
+	normalize(vec3(0, 0, 1)),
+
+	normalize(vec3(1, 1, 1 )),
+	normalize(vec3(-1, 1, 1)),
+	normalize(vec3(1, -1, 1)),
+	normalize(vec3(1, 1, -1)),
+
+	normalize(vec3(0, 1, PHI+1)),
+	normalize(vec3(0, -1, PHI+1)),
+	normalize(vec3(PHI+1, 0, 1)),
+	normalize(vec3(-PHI-1, 0, 1)),
+	normalize(vec3(1, PHI+1, 0)),
+	normalize(vec3(-1, PHI+1, 0)),
+
+	normalize(vec3(0, PHI, 1)),
+	normalize(vec3(0, -PHI, 1)),
+	normalize(vec3(1, 0, PHI)),
+	normalize(vec3(-1, 0, PHI)),
+	normalize(vec3(PHI, 1, 0)),
+	normalize(vec3(-PHI, 1, 0))
+);
+
+// Version with variable exponent.
+// This is slow and does not produce correct distances, but allows for bulging of objects.
+float fGDF(vec3 p, float r, float e, int begin, int end) {
+	float d = 0;
+	for (int i = begin; i <= end; ++i)
+		d += pow(abs(dot(p, GDFVectors[i])), e);
+	return pow(d, 1/e) - r;
+}
+
+// Version with without exponent, creates objects with sharp edges and flat faces
+float fGDF(vec3 p, float r, int begin, int end) {
+	float d = 0;
+	for (int i = begin; i <= end; ++i)
+		d = max(d, abs(dot(p, GDFVectors[i])));
+	return d - r;
+}
+
+// Primitives follow:
+
+float fOctahedron(vec3 p, float r, float e) {
+	return fGDF(p, r, e, 3, 6);
+}
+
+float fDodecahedron(vec3 p, float r, float e) {
+	return fGDF(p, r, e, 13, 18);
+}
+
+float fIcosahedron(vec3 p, float r, float e) {
+	return fGDF(p, r, e, 3, 12);
+}
+
+float fTruncatedOctahedron(vec3 p, float r, float e) {
+	return fGDF(p, r, e, 0, 6);
+}
+
+float fTruncatedIcosahedron(vec3 p, float r, float e) {
+	return fGDF(p, r, e, 3, 18);
+}
+
+float fOctahedron(vec3 p, float r) {
+	return fGDF(p, r, 3, 6);
+}
+
+float fDodecahedron(vec3 p, float r) {
+	return fGDF(p, r, 13, 18);
+}
+
+float fIcosahedron(vec3 p, float r) {
+	return fGDF(p, r, 3, 12);
+}
+
+float fTruncatedOctahedron(vec3 p, float r) {
+	return fGDF(p, r, 0, 6);
+}
+
+float fTruncatedIcosahedron(vec3 p, float r) {
+	return fGDF(p, r, 3, 18);
+}
