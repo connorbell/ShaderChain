64a7e54d3a91daeefeb6426abcef110ea1241451
Connor Bell <connor.n.bell@gmail.com>
2020-04-02 10:02:44 -0400

    Added hue shift shader

---
+++ bin/data/includes/hue.glsl
@@ -0,0 +1,7 @@
+// https://gist.github.com/mairod/a75e7b44f68110e1576d77419d608786#gistcomment-3195243
+
+vec3 hueShift(vec3 color, float hue) {
+    const vec3 k = vec3(0.57735, 0.57735, 0.57735);
+    float cosAngle = cos(hue);
+    return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
+}
